8d84175a0bbfbb954795581ef9bbea4de5e4d573
kikis19 <terbrova.kristyna@gmvv.cz>
2025-05-30 11:51:03 +0200

    .

--- script.js
+++
@@ -1,9 +0,0 @@
-// Tady můžeš později přidat funkce pro rozklikávání produktů nebo ukládání položek do košíku.
-// Prozatím jen jednoduchý skript pro načítání kategorie.
-
-const categories = document.querySelectorAll('.category-box');
-categories.forEach(category => {
-    category.addEventListener('click', () => {
-        alert('Našel jsi kategorii: ' + category.id);
-    });
-});
